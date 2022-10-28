ab6f7f1fa3dd136b9f5e7b1d3c04fdeaad40494f
to1712 <toto.orrico@gmail.com>
2022-10-28 16:42:42 +0200

    commit

---
+++ ProvaIngsw/src/main/java/provaingsw/Main.java
@@ -0,0 +1,9 @@
+package provaingsw;
+
+public class Main {
+	public static void main(String[] args) {
+		FunnyAlgorithms funny=new FunnyAlgorithms();
+		int we=funny.stringToIntConverter("1234");
+		System.out.println(we);
+	}
+}
